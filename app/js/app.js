ed491f159e32070867454f13b4ae815a8d0e1133
pnk0011 <pnk0011@gmail.com>
2019-12-12 10:31:47 -1200

    final commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,4 @@
-var myApp = angular.module('myApp', ['ui.bootstrap'])
-
+var myApp = angular.module('myApp', ['ui.bootstrap', 'infinite-scroll'])
 myApp.directive('starRating', function () {
   return {
     restrict: 'A',
@@ -23,59 +22,30 @@ myApp.directive('starRating', function () {
   }
 });
 
-myApp.directive('starRatingDynamic', function () {
-  return {
-    restrict: 'A',
-    template: '<ul class="rating">' +
-      '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
-      '\u2605' +
-      '</li>' +
-      '</ul>',
-    scope: {
-      ratingValue: '=',
-      max: '=',
-      onRatingSelected: '&'
-    },
-    link: function (scope, elem, attrs) {
-
-      var updateStars = function () {
-        scope.stars = [];
-        for (var i = 0; i < scope.max; i++) {
-          scope.stars.push({
-            filled: i < scope.ratingValue
-          });
-        }
-      };
-
-      scope.toggle = function (index) {
-        scope.ratingValue = index + 1;
-        scope.onRatingSelected({
-          rating: index + 1
-        });
-      };
-
-      scope.$watch('ratingValue', function (oldVal, newVal) {
-        if (newVal) {
-          updateStars();
-        }
-      });
-    }
-  }
-});
-
 myApp.controller('MovieController', ["$scope", "imdbService", function ($scope, imdbService) {
   var paramObj = {};
   $scope.posters = [];
+  $scope.ratingArray = [];
+  $scope.modalData = [];
   $scope.submitted = false;
   $scope.putLoader = false;
-  var pendingTask;
+  $scope.maxRating = 10;
+  $scope.ratedBy = 0;
+
+  $scope.rateBy = function (star) {
+    $scope.ratedBy = star;
+    $scope.ratingArray[($scope.modalData.imdbID)] = star;
+
+  }
+
   $scope.onImageClick = function (poster) {
+    someData = poster;
+    $scope.modalData = someData;
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
+    //var captionText = document.getElementById("caption");
     $scope.modalTitle = poster.Title;
     $scope.modalYear = poster.Year;
-
     $scope.modalPlot = poster.Plot;
     modal.style.display = "block";
     modalImg.src = poster.Poster;
@@ -86,32 +56,26 @@ myApp.controller('MovieController', ["$scope", "imdbService", function ($scope, 
     }
     $scope.ratings = [];
     $scope.ratings[0] = {};
-    $scope.ratingsDynamic = [];
-    $scope.ratingsDynamic[0] = {};
-
     $scope.rating = 0;
     $scope.ratings = [{
-
       max: 10
     }];
-    $scope.ratingsDynamic = [{
-      max: 10
-    }];
-
     $scope.ratings[0].current = parseInt(poster.imdbRating);
-    $scope.ratingsDynamic[0].current = 1;
+    if ($scope.ratingArray[(poster.imdbID)] == undefined) {
+      $scope.ratingArray[(poster.imdbID)] = 0;
+      $scope.ratedBy = 0;
+    } else {
+      $scope.ratedBy = $scope.ratingArray[(poster.imdbID)];
+    }
   }
 
   $scope.getMovieData = function (movieID) {
     $scope.putLoader = true;
     paramObj = {
       i: movieID
-
     };
     fetchSelectedMovieData(paramObj);
   };
-
-
   $scope.fetchDetails = function () {
     if ($scope.omdbSearch.$valid) {
       $scope.putLoader = true;
@@ -147,18 +111,26 @@ myApp.controller('MovieController', ["$scope", "imdbService", function ($scope, 
 
   }
 
+  var pageCount = 1;
+
+  $scope.loadMore = function () {
+    if ($scope.putLoader == false && $scope.posters.length > 0) {
+      pageCount += 1;
+      paramObj.page = pageCount;
+      fetch(paramObj);
+      $scope.putLoader = true;
+    }
+  }
 
   $scope.posters = [];
+
   function fetch(paramObj) {
-
-
     imdbService.events(paramObj).success(function (resp) {
-    $scope.details = resp; console.log(resp);
-      if (resp.Response == 'True') {
+      $scope.details = resp; console.log(resp);
+      if (resp.Response == 'True' && resp.Search != undefined) {
         for (var i = 0; i < resp.Search.length; i++) {
           if (resp.Search[i].Poster != 'N/A')
             $scope.posters.push(resp.Search[i]);
-
         }
       }
       console.log($scope.posters);
@@ -171,10 +143,9 @@ myApp.controller('MovieController', ["$scope", "imdbService", function ($scope, 
   $scope.selectedMovieData = [];
   function fetchSelectedMovieData(paramObj) {
     imdbService.events(paramObj).success(function (resp) {
-    $scope.details = resp; console.log(resp); $scope.onImageClick(resp);
+      $scope.details = resp; console.log(resp); $scope.onImageClick(resp);
       $scope.putLoader = false;
     });
   }
 
-  
 }]);
